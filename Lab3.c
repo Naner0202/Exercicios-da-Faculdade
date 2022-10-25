743461214d2e4ec4869f8d1c8f7de0e824bdfa74
Rennan Silva <111655174+Rennan0102@users.noreply.github.com>
2022-10-25 18:31:00 -0300

    Update Lab3.c

--- Lab3.c
+++ Lab3.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
-//int main() {
+int main() {
   // Exercício 1 - Faça um Programa que peça um valor e mostre na tela se o valor é um múltiplo de 3, 4 ou 5.
 
-  /*int num;
+  int num;
   printf("Informe um número: ");
   scanf("%d", &num);
 
@@ -15,11 +15,11 @@
   }
   if (num % 5 == 0) {
     printf("%d é múltiplo de 5\n", num);
-  }*/
+  }
 
   // Exercício 2 - Reescreva a solução do desafio 2 do lab anterior com o uso de operador ternário. Substitua as possíveis entradas por números (1-Matutino, 2-Vespertino e 3-Noturno).
 
-  /*int turno;
+  int turno;
 
   printf("------Menu------\n\n");
   printf("1 - Matutino\n");
@@ -29,11 +29,11 @@
   scanf("%d", &turno);
 
   turno == 1 ? printf("Bom dia!") : turno == 2 ? printf("Boa tarde") : turno ==
-  3 ? printf("Boa noite") : printf("Valor inválido");*/
+  3 ? printf("Boa noite") : printf("Valor inválido");
 
   // Exercício 3 - Faça um programa para a leitura de duas notas parciais de um aluno. O programa deve calcular a média alcançada por aluno e apresentar: A mensagem "Aprovado", se a média alcançada for maior ou igual a sete; A mensagem "Reprovado", se a média for menor do que sete; A mensagem "Aprovado com Distinção", se a média for igual a dez.
 
-  /*float nota1, nota2;
+  float nota1, nota2;
   printf("Informe a 1° Nota: ");
   scanf("%f", &nota1);
   printf("Informe a 2° Nota: ");
@@ -47,12 +47,12 @@
     printf("Aprovado!");
   } else if (media < 7) {
     printf("Reprovado!");
-  }*/
+  }
 // São cinco opções: consultar saldo, fazer empréstimo, consultar fatura do cartão de crédito, pedir novo cartão ou falar diretamente com atendente, sendo esta última, a opção padrão.
 
   // Exercício 4 Simule, com a ajuda de um switch, um atendimento telefônico em um banco. São cinco opções: consultar saldo, fazer empréstimo, consultar fatura do cartão de crédito, pedir novo cartão ou falar diretamente com atendente, sendo esta última, a opção padrão.
 
-  /*int op;
+  int op;
   
   printf("\n------ Rennan's Bank ------\n");
   printf("1 - Consultar saldo\n");
@@ -67,11 +67,11 @@
     case 3: printf("2° Via da fatura do cartão de crédito: R$ 2800,00"); break;
     case 4: printf("Cartão novo chegará em 2 dias utéis!"); break;
     default: printf("Fale diretamente com um dos nossos atendentes"); break;
-  }*/
+  }
 
   // Desafio 1 - Faça um programa que lê as duas notas parciais obtidas por um aluno numa disciplina ao longo de um semestre, e calcule a sua média. O algoritmo deve mostrar na tela as notas, a média, o conceito correspondente e a mensagem “APROVADO” se o conceito for A, B ou C ou “REPROVADO” se o conceito for D ou E.
 
-  /*float nota1, nota2;
+  float nota1, nota2;
   int conc;
   printf("Informe a 1° Nota: ");
   scanf("%f", &nota1);
@@ -105,7 +105,7 @@
     case 3: printf("Aprovado!"); break;
     case 4: printf("Aprovado!"); break;
     case 5: printf("Aprovado!"); break;
-  }*/
+  }
   
-  //return 0;
-//}
+  return 0;
+}
