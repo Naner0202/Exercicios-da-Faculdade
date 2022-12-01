3d90874eba9af9cfe59d9e0950df6858eb82c1a4
Rennan Silva <111655174+Rennan0102@users.noreply.github.com>
2022-12-01 14:57:09 -0300

    Update Lab9.c

--- Lab9.c
+++ Lab9.c
@@ -5,36 +5,36 @@
 void calculaAreaPerimetro(float *ptrRaio);
 void contarVogaisConsoantes(char *sPtr);
 
-//int main(){
+int main(){
   // Exercício 1 - Crie um programa que recebe dois valores inteiros n1 e n2 e, utilizando ponteiros p1 e p2, calcula as seguintes expressões: *p1 - *p2 | **&p1 | 3* - *p1/(*p2)+7
 
-  /*int num1, num2;
+  int num1, num2;
   int *p1 = &num1, *p2 = &num2;
   printf("Informe o 1° valor: ");
   scanf("%d", &num1);
   printf("Informe o 2° valor: ");
   scanf("%d", &num2);
 
-  printf("*p1 - *p2 = %d\n**&p1 = %d\n3 * -*p1 / (*p2) + 7 = %d", *p1 - *p2, **&p1, 3 * -*p1 / (*p2) + 7);*/
+  printf("*p1 - *p2 = %d\n**&p1 = %d\n3 * -*p1 / (*p2) + 7 = %d", *p1 - *p2, **&p1, 3 * -*p1 / (*p2) + 7);
 
   // Exercício 2 - Utilizando a mesma lógica do exemplo anterior, utilizando passagem por referência, crie uma função que, recebendo do usuário o valor do raio, calcula o perímetro (2*pi*raio) e a área (pi*raio²) de um círculo. Considere pi = 3.14.
   
-  /*float raio;
+  float raio;
   printf("Informe o raio do círculo: ");
   scanf("%f", &raio);
   
-  calculaAreaPerimetro(&raio);*/
+  calculaAreaPerimetro(&raio);
 
   // Desafio 1 -  Elabore uma função que recebe uma string e retorna quantas vogais e quantas consoantes ela possui. Lembre-se de utilizar passagem de parâmetro como referência.
   
-  /*char palavra[50];
+  char palavra[50];
   printf("Digite uma palavra: ");
   scanf("%s", palavra);
   
-  contarVogaisConsoantes(palavra);*/
+  contarVogaisConsoantes(palavra);
 
-  //return 0;
-//}
+  return 0;
+}
 
 void calculaAreaPerimetro(float *ptrRaio){
   // Procedimento para o Exercício 2
